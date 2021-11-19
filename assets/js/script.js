e7064e31343d61e9d7bf02cea4da668e5ccc74d7
John Hur <hur.john628@gmail.com>
2021-11-19 06:43:53 -0600

    added contact me and added more style

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,20 @@ const aboutMeEl = document.querySelector("#aboutMe");
 const workEl = document.querySelector("#work");
 const resumeEl = document.querySelector("#resume");
 const linksEl = document.querySelector("#links");
+const contactMeEl = document.querySelector("#contactMe");
 
 const aboutMeButton = document.querySelector("#aboutMeButton");
 const workButton = document.querySelector("#workButton");
 const resumeButton = document.querySelector("#resumeButton");
 const linksButton = document.querySelector("#linksButton");
+const contactMeButton = document.querySelector("#contactMeButton")
 
 aboutMeButton.addEventListener("click", function() {
     aboutMeEl.style.display = "flex";
     workEl.style.display = "none";
     linksEl.style.display = "none";
     resumeEl.style.display = "none";
+    contactMeEl.style.display = "none";
 })
 
 workButton.addEventListener("click", function() {
@@ -20,6 +23,7 @@ workButton.addEventListener("click", function() {
     workEl.style.display = "block";
     linksEl.style.display = "none";
     resumeEl.style.display = "none";
+    contactMeEl.style.display = "none";
 })
 
 linksButton.addEventListener("click", function() {
@@ -27,6 +31,7 @@ linksButton.addEventListener("click", function() {
     workEl.style.display = "none";
     linksEl.style.display = "flex";
     resumeEl.style.display = "none";
+    contactMeEl.style.display = "none";
 })
 
 resumeButton.addEventListener("click", function() {
@@ -34,4 +39,13 @@ resumeButton.addEventListener("click", function() {
     workEl.style.display = "none";
     linksEl.style.display = "none";
     resumeEl.style.display = "flex";
+    contactMeEl.style.display = "none";
+})
+
+contactMeButton.addEventListener("click", function() {
+    aboutMeEl.style.display = "none";
+    workEl.style.display = "none";
+    linksEl.style.display = "none";
+    resumeEl.style.display = "none";
+    contactMeEl.style.display = "flex";
 })
