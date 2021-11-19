f36af959cab97b07f97dd62539dc105e4bbeffac
John Hur <hur.john628@gmail.com>
2021-11-18 23:33:27 -0600

    added links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,25 @@
 const aboutMeEl = document.querySelector("#aboutMe");
 const workEl = document.querySelector("#work");
+const linksEl = document.querySelector("#links");
 
 const aboutMeButton = document.querySelector("#aboutMeButton");
 const workButton = document.querySelector("#workButton");
+const linksButton = document.querySelector("#linksButton");
 
 aboutMeButton.addEventListener("click", function() {
     aboutMeEl.style.display = "flex";
     workEl.style.display = "none";
+    linksEl.style.display = "none";
 })
 
 workButton.addEventListener("click", function() {
     aboutMeEl.style.display = "none";
     workEl.style.display = "block";
+    linksEl.style.display = "none";
+})
+
+linksButton.addEventListener("click", function() {
+    aboutMeEl.style.display = "none";
+    workEl.style.display = "none";
+    linksEl.style.display = "flex";
 })
